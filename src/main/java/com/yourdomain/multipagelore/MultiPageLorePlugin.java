4233914ff95f3941597d4166ad7c8a9000f3bb72
Bakayarouz <56614842+Bakayarouz@users.noreply.github.com>
2026-09-20 10:43:31 +0700

    Update MultiPageLorePlugin.java

--- src/main/java/com/yourdomain/multipagelore/MultiPageLorePlugin.java
+++ src/main/java/com/yourdomain/multipagelore/MultiPageLorePlugin.java
@@ -1,6 +1,8 @@
 package com.yourdomain.multipagelore;
 
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MultiPageLorePlugin extends JavaPlugin {
@@ -11,11 +13,26 @@ public class MultiPageLorePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        saveDefaultConfig(); 
+
         PAGES_KEY = new NamespacedKey(this, "lore_pages");
         CURRENT_PAGE_KEY = new NamespacedKey(this, "current_page");
         MAX_WIDTH_KEY = new NamespacedKey(this, "max_width");
         
+        LoreManager.init(this);
+        
         getServer().getPluginManager().registerEvents(new MultiPageListener(this), this);
-        getLogger().info("MultiPageLore (Enterprise Optimized) enabled.");
+
+        // UNIVERSAL ITEM CATCHER: Runs every 10 ticks (0.5 seconds).
+        // Catches items given by console commands, MythicMobs, crates, or other plugins.
+        getServer().getScheduler().runTaskTimer(this, () -> {
+            for (Player player : getServer().getOnlinePlayers()) {
+                for (ItemStack item : player.getInventory().getContents()) {
+                    LoreManager.bakeItemIfNeeded(item);
+                }
+            }
+        }, 10L, 10L);
+
+        getLogger().info("MultiPageLore enabled with universal item scanning.");
     }
 }
