5694a9f71ee9d944a6c3136f2359fa418ed79db6
Bakayarouz <56614842+Bakayarouz@users.noreply.github.com>
2026-09-20 12:50:53 +0700

    Update MultiPageLorePlugin.java

--- src/main/java/com/yourdomain/multipagelore/MultiPageLorePlugin.java
+++ src/main/java/com/yourdomain/multipagelore/MultiPageLorePlugin.java
@@ -5,24 +5,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class MultiPageLorePlugin extends JavaPlugin {
 
-    // The keys used to store hidden data (NBT/PersistentDataContainer) on the items
     public static NamespacedKey PAGES_KEY;
     public static NamespacedKey CURRENT_PAGE_KEY;
 
     @Override
     public void onEnable() {
-        // Generates the default config.yml if it doesn't exist yet
         saveDefaultConfig();
 
-        // Initialize the NamespacedKeys (must be done after the plugin initializes)
         PAGES_KEY = new NamespacedKey(this, "pages_data");
         CURRENT_PAGE_KEY = new NamespacedKey(this, "current_page_index");
 
-        // Pass the plugin instance to the LoreManager so it can read the config
         LoreManager.init(this);
 
-        // Register our main listener
+        // Register event listener
         getServer().getPluginManager().registerEvents(new MultiPageListener(this), this);
+
+        // Register command and tab completer
+        if (getCommand("multipagelore") != null) {
+            MultiPageCommand cmdHandler = new MultiPageCommand(this);
+            getCommand("multipagelore").setExecutor(cmdHandler);
+            getCommand("multipagelore").setTabCompleter(cmdHandler);
+        }
 
         getLogger().info("MultiPageLore has been successfully enabled!");
     }
