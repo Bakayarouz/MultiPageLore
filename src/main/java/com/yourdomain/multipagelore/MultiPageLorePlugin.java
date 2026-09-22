cb587341a698bef2651d459419adbcf5b46e7601
Bakayarouz <56614842+Bakayarouz@users.noreply.github.com>
2026-09-22 16:57:31 +0700

    Update MultiPageLorePlugin.java

--- src/main/java/com/yourdomain/multipagelore/MultiPageLorePlugin.java
+++ src/main/java/com/yourdomain/multipagelore/MultiPageLorePlugin.java
@@ -1,7 +1,12 @@
 package com.yourdomain.multipagelore;
 
 import org.bukkit.NamespacedKey;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MultiPageLorePlugin extends JavaPlugin {
@@ -11,7 +16,8 @@ public class MultiPageLorePlugin extends JavaPlugin {
     public static NamespacedKey FOOTER_KEY;
     public static NamespacedKey CURRENT_PAGE_KEY;
 
-    @Override
+    private MultiPageListener listener;
+
     public void onEnable() {
         saveDefaultConfig();
 
@@ -22,21 +28,18 @@ public class MultiPageLorePlugin extends JavaPlugin {
 
         LoreManager.init(this);
 
-        // Register event listener
-        getServer().getPluginManager().registerEvents(new MultiPageListener(this), this);
+        listener = new MultiPageListener(this);
+        getServer().getPluginManager().registerEvents((Listener) listener, (Plugin) this);
 
-        // Register command and tab completer
         if (getCommand("multipagelore") != null) {
             MultiPageCommand cmdHandler = new MultiPageCommand(this);
-            getCommand("multipagelore").setExecutor(cmdHandler);
-            getCommand("multipagelore").setTabCompleter(cmdHandler);
+            getCommand("multipagelore").setExecutor((CommandExecutor) cmdHandler);
+            getCommand("multipagelore").setTabCompleter((TabCompleter) cmdHandler);
         }
 
-        // Background Inventory Scanner: Automatically pre-bakes items given via /mi give or plugins within 0.5 seconds
-        getServer().getScheduler().runTaskTimer(this, () -> {
-            for (org.bukkit.entity.Player player : getServer().getOnlinePlayers()) {
-                ItemStack[] contents = player.getInventory().getContents();
-                for (ItemStack item : contents) {
+        getServer().getScheduler().runTaskTimer((Plugin) this, () -> {
+            for (Player player : getServer().getOnlinePlayers()) {
+                for (ItemStack item : player.getInventory().getContents()) {
                     if (item != null && !item.isEmpty()) {
                         LoreManager.bakeItemIfNeeded(item);
                     }
@@ -47,8 +50,11 @@ public class MultiPageLorePlugin extends JavaPlugin {
         getLogger().info("MultiPageLore has been successfully enabled!");
     }
 
-    @Override
     public void onDisable() {
         getLogger().info("MultiPageLore has been disabled.");
+    }
+
+    public MultiPageListener getListener() {
+        return listener;
     }
 }
